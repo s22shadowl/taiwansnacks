263c699d0c235ba41a9a943ba9a71f88cfbcb323
s22shadowl <jjerry409@gmail.com>
2015-06-14 18:51:38 +0800

    9/9

---
+++ hello.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+
+echo"<center>";
+
+echo "<h1>九九乘法練習。</h1><br>";
+
+echo "<body bgcolor=#00FFFF ";
+
+
+
+for ($i=1; $i < 10 ; $i++) {
+for ($ii=1; $ii < 10 ; $ii++) { 	
+if ($i*$ii < 10) {
+	echo "$i*$ii =   "."0"."$i"*"$ii"."&nbsp&nbsp&nbsp";   
+ } else {
+ 	echo "$i*$ii =   "." $i"*"$ii"."&nbsp&nbsp&nbsp";   
+ }
+  
+}
+echo "<br>";
+echo "<hr size=2 align=center width=100%>";
+}
+
+echo"</center>";
